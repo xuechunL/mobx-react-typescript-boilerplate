17b0b90c20f4b3d45ece40a3961fa749a73b8b52
xuechun Lei <xuechunlei922@gmail.com>
2018-11-20 19:13:35 +0800

    *: add a simple demo for @observer, @observable, @computed of mobx and mobx-react

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { observable } from 'mobx'
+import { observable, computed } from 'mobx'
 import { observer } from 'mobx-react'
 import DevTools from 'mobx-react-devtools'
 
@@ -34,5 +34,46 @@ class TimerView extends React.Component<{appState: AppState}, {}> {
 	}
 }
 
+class OrderLineStore {
+	@observable price = 0
+	@observable amount = 1
+
+	constructor(price, amount?: number) {
+		this.price = price
+		this.amount = amount || 1
+	}
+
+	@computed get total() {
+		return this.price * this.amount
+	}
+}
+
+@observer
+class OrderView extends React.Component<{orderStore: OrderLineStore}, {}> {
+	render() {
+		return (
+			<div>
+					<p>
+						price: {this.props.orderStore.price}, amount: {this.props.orderStore.amount}
+					</p>
+					<p>
+					total:
+					{this.props.orderStore.price} * {this.props.orderStore.amount} = {this.props.orderStore.total}
+					</p>
+				<DevTools />
+			</div>
+		)
+	}
+}
+
 const appState = new AppState()
-ReactDOM.render(<TimerView appState={appState} />, document.getElementById('root'))
+const orderStore = new OrderLineStore(12)
+const orderStore2 = new OrderLineStore(12, 8)
+
+ReactDOM.render((
+	<div>
+		<TimerView appState={appState} />
+		<OrderView orderStore={orderStore} />
+		<OrderView orderStore={orderStore2} />
+	</div>
+), document.getElementById('root'))
